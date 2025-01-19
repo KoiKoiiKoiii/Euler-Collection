e818ff33630ec27c278cff1f9b4f7771c7083399
KoiKoiiKoiii <caninenote@gmail.com>
2025-01-19 16:26:53 -0500

    Update Euler1.py
    Added a simple [maximum] to the multipleList function so the user knows how much they're trying to find.

--- Euler1.py
+++ Euler1.py
@@ -1,13 +1,13 @@
-def multipleList():
+def multipleList(maximum):
   multiples = []
-  for i in range(1, 10000):
+  for i in range(1, maximum):
     if (i % 3 == 0 or i % 5 == 0):
       multiples.append(i)
   return multiples
 
 def sumOf():
   new = []
-  for i in multipleList():
+  for i in multipleList(10000):
     if (i < 1000):
       new.append(i)
     else:
